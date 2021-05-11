bab5c5920dc84d93d9e451f219be7167a17f2361
Alexander Popov <65553615+alexpopov1@users.noreply.github.com>
2021-05-11 09:44:22 +0100

    Add files via upload

---
+++ Formation/SpineSweep.jl
@@ -0,0 +1,222 @@
+
+
+include("FormationModel.jl")
+include("DataTransfer.jl")
+include("WarmStart.jl")
+
+
+
+
+
+
+
+function new_model(X_fixed::Dict, nhood::Array, parameters::Tuple)
+
+    num, _, N = parameters
+
+    fixed_agents = keys(X_fixed)
+    variable_agents = setdiff(nhood, fixed_agents)
+
+    if isempty(variable_agents)
+
+        model = Model()
+
+    else
+
+        agents = union(fixed_agents, variable_agents)
+
+        model = base_model(variable_agents[1], parameters)
+        update_model(model, variable_agents, [variable_agents[1]], parameters)
+
+        x, u = model[:x], model[:u]
+
+        mixed_pairs = []
+        pairs = pairing(agents)
+        for pair in pairs
+            if (pair[1] in fixed_agents && pair[2] in variable_agents) ||
+               (pair[1] in variable_agents && pair[2] in fixed_agents)
+                push!(mixed_pairs, pair)
+            end
+        end
+
+        vars_and_constants = Dict()
+        for i in agents
+            if i in fixed_agents
+                vars_and_constants[i] = X_fixed[i]
+            else
+                vars_and_constants[i] = x[i]
+            end
+        end
+
+        new_cons = coupled_inequalities(vars_and_constants, mixed_pairs, parameters)
+        @constraint(model, vcat(new_cons...) .<= 0)
+
+    end
+
+    return model
+
+end
+
+
+
+
+
+
+
+
+function solve_problem(model::Model, x_fixed::Dict, u_fixed::Dict, 
+                       nhood::Array, parameters::Tuple, prev_solutions::Dict)
+
+
+    fixed_agents = keys(x_fixed)
+    variable_agents = setdiff(nhood, fixed_agents)
+
+    if length(variable_agents) >= 1
+  
+        if length(intersect(fixed_agents, nhood)) == 0
+            warm_start(model, parameters, nhood)
+        else
+            warm_start(model, variable_agents, prev_solutions)
+        end
+
+        optimise_model(model)
+
+    end
+
+    X_dict, U_dict = Dict(), Dict()
+
+    for j in nhood
+        X_dict[j], U_dict[j] = j in keys(X_fixed) ? (X_fixed[j], U_fixed[j]) : (value.(model[:x][j]), value.(model[:u][j]))
+    end
+
+    return X_dict, U_dict
+
+end
+
+
+
+
+
+
+
+
+
+
+function spinesweep(sys::Int, parameters::Tuple, neighbours::Array;
+                    agent_procs::Dict = Dict(i=>sort(workers())[i] for i = 1:parameters[1]))
+
+    # Define constant parameters
+    num, _, N = parameters
+    nhood = [([sys, neighbours]...)...]
+
+    # Create model
+    model = model_setup(sys, parameters, nhood)
+
+    # Initialise variables
+    x_solution, u_solution = Array{Float64, 2}(undef, 4, N+1), Array{Float64, 2}(undef, 2, N+1)
+    NODE, SOLVER, PASSER, FETCHER = false, false, false, false
+
+    # Initialise communication
+    if sys in path
+
+        SOLVER = true
+        NODE = position == 1 ? true : false
+        position = findall(x->x==sys, path)
+        sender = position == 1 ? 0 : agent_procs[path[position-1]]
+        x_fixed, u_fixed, new_x_fixed, new_u_fixed, prev_solutions = Dict(), Dict(), Dict(), Dict(), Dict()
+
+        receivers = setdiff(nhood, path)
+        for j in receivers
+            Core.eval(Main, Expr(:(=), Symbol("chx", j), Channel{Any}(1)))
+        end
+
+        if position < length(path)
+            PASSER = true
+            global relay = Channel{Any}(1) 
+        end
+
+    else
+
+        FETCHER = true
+        links = intersect(nhood, path)
+        order = Dict(i=>findall(x->x==i, path) for i in links)
+        sender = agent_procs[path[minimum(values(order))]]
+
+    end
+      
+    
+    # BEGIN TIMING TOTAL TIME IN WHILE LOOP
+    totalloop = @elapsed begin
+
+    while true
+
+        if SOLVER      # solve problem with unfixed variables of neighbourhood and all fixed solutions
+            X_dict, U_dict = solve_problem(model, x_fixed, u_fixed, nhood, parameters, prev_solutions)
+        end
+
+        # if node, pass all new fixed solutions forward in path (and check if solved - for testing purposes)
+        if SOLVER
+
+            if NODE
+
+                for j in setdiff(nhood, fixed)
+                    x_fixed[j], u_fixed[j] = X_dict[j], U_dict[j]
+                    new_x_fixed[j], new_u_fixed[j] = X_dict[j], U_dict[j]
+                end
+
+                for j in receivers
+                    put!(getfield(Main, Symbol("chx", j)), (X_dict[j], U_dict[j]))
+                end
+
+                if PASSER
+                    put!(relay, (new_x_fixed, new_u_fixed, true))
+                end
+
+                x_solution, u_solution = X_dict[sys], U_dict[sys]
+                break
+
+            else
+
+                remotecall_fetch(wait, sender, getfield(Main, :relay))
+                new_x_fixed, new_u_fixed, NODE = fetch(@spawnat(sender, take!(getfield(Main, :relay))))
+
+                for j in keys(new_x_fixed)
+                    x_fixed[j] = new_x_fixed[j]
+                end
+             
+                for j in keys(new_u_fixed)
+                    u_fixed[j] = new_u_fixed[j]
+                end
+
+                prev_solutions = Dict()
+                for j in setdiff(nhood, keys(x_fixed))
+                    prev_solutions[j] = (X_dict[j], U_dict[j])
+                end
+
+                if PASSER
+                    put!(relay, (new_x_fixed, new_u_fixed, false))
+                end
+      
+            end
+
+        elseif FETCHER
+
+            remotecall_fetch(wait, sender, @spawnat(sender, getfield(Main, Symbol("chx", sys))))
+            x_solution, u_solution = fetch(@spawnat(sender, take!(getfield(Main, Symbol("chx", sys)))))
+            break
+
+        end
+            
+
+    end
+
+    # STOP TIMING TOTAL TIME IN WHILE LOOP
+    end
+
+    return x_solution, u_solution
+
+end
+
+
+    
+
